9e8838d5124147c28ed8955554f6954e254d8644
keya12345 <0319452930@student.wub.edu.bd>
2022-03-26 00:28:03 +0600

    second commit image

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -4,7 +4,7 @@ const Product = (props) => {
     console.log(props)
     return (
         <div>
-          <h2>{props.product.name}</h2>  
+          <h2>{props.product.image}</h2>  
         </div>
     );
 };
